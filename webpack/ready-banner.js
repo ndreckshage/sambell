4e0f18e79e041a08549eef75d17893aa85b87b78
Nick Dreckshage <nicholasdreckshage@gmail.com>
2017-05-21 07:29:29 -0500

    update

--- webpack/ready-banner.js
+++ webpack/ready-banner.js
@@ -1,13 +1,9 @@
-// if (typeof window !== "undefined") {
-//   setTimeout(function() {
-//     if (!window.__SAMBELL_CHUNK_COUNT__) {
-//       window.__SAMBELL_CHUNK_COUNT__ = 0;
-//     }
-//     window.__SAMBELL_CHUNK_COUNT__ = window.__SAMBELL_CHUNK_COUNT__ + 1;
-//     if (typeof window.__SAMBELL_CHUNK_CB__ === "function") {
-//       window.__SAMBELL_CHUNK_CB__();
-//     }
-//   }, 0);
-// }
-
-module.exports = '"undefined"!=typeof window&&setTimeout(function(){window.__SAMBELL_CHUNK_COUNT__||(window.__SAMBELL_CHUNK_COUNT__=0),window.__SAMBELL_CHUNK_COUNT__=window.__SAMBELL_CHUNK_COUNT__+1,"function"==typeof window.__SAMBELL_CHUNK_CB__&&window.__SAMBELL_CHUNK_CB__()},0);';
+module.exports = `(function(){
+  if (typeof window !== "undefined") {
+    setTimeout(function() {
+      if (!window.__SAMBELL_CHUNK_COUNT__) window.__SAMBELL_CHUNK_COUNT__ = 0;
+      window.__SAMBELL_CHUNK_COUNT__ = window.__SAMBELL_CHUNK_COUNT__ + 1;
+      if (typeof window.__SAMBELL_CHUNK_CB__ === "function") window.__SAMBELL_CHUNK_CB__();
+    }, 0);
+  }
+})();`;
