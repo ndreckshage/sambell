9ec1439852988163ba6925c389b81d98765f7cc4
Nick Dreckshage <nicholasdreckshage@gmail.com>
2017-05-21 07:32:14 -0500

    update

--- webpack/sambell-ready.js
+++ webpack/sambell-ready.js
@@ -7,6 +7,7 @@ const chunkCount = () => window.__SAMBELL_CHUNK_COUNT__ || 0;
 const chunksReady = () => chunkCount() >= waitForChunks;
 
 const maybeLoad = () => {
+  console.log('ml', clientCalledReady, chunkCount, chunksReady())
   if (!ready && clientCalledReady && clientCb && chunksReady()) {
     ready = true;
     clientCb();
@@ -16,6 +17,7 @@ const maybeLoad = () => {
 window.__SAMBELL_CHUNK_CB__ = maybeLoad;
 
 export default cb => {
+  console.log('client called');
   clientCalledReady = true;
   clientCb = cb;
   maybeLoad();
