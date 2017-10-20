2860253004324772d9c11ccad2ee4c1e6eb686da
Nick Dreckshage <nicholasdreckshage@gmail.com>
2017-10-20 17:08:41 -0500

    bump versions; new styled jsx

--- template/components/Moon.js
+++ template/components/Moon.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
-export default () =>
+const Moon = props => (
   <div>
     <p className="WakeMe">Wake me</p>
     <p className="WhenIts">{`When it's quitting time`}</p>
     <Link to="/outside">Go outside.</Link>
     <style jsx>{`
       .WakeMe {
-        color: #B71C1C;
+        color: ${props.color};
         font-size: 8rem;
         margin: 0;
       }
@@ -18,3 +18,10 @@ export default () =>
       }
     `}</style>
   </div>
+);
+
+Moon.defaultProps = {
+  color: '#B71C1C'
+};
+
+export default Moon;
